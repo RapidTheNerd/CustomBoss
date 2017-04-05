f7ac967fe9a02dbf876360921c6f3081b9961733
RapidTheNerd <george.marr.cobc@gmail.com>
2017-04-05 23:11:49 +0100

    should work...i think...i should really test this

--- src/main/java/me/rtn/cb/CustomEntity.java
+++ src/main/java/me/rtn/cb/CustomEntity.java
@@ -1,9 +1,11 @@
 package me.rtn.cb;
 
-import net.minecraft.server.v1_11_R1.EntityZombie;
-import net.minecraft.server.v1_11_R1.GenericAttributes;
-import net.minecraft.server.v1_11_R1.World;
+import net.minecraft.server.v1_11_R1.*;
 import org.bukkit.ChatColor;
+import org.bukkit.Location;
+import org.bukkit.craftbukkit.v1_11_R1.CraftWorld;
+import org.bukkit.craftbukkit.v1_11_R1.entity.CraftLivingEntity;
+import org.bukkit.entity.Zombie;
 
 /**
  * Created by George on 05-Apr-17 on Apr at 11:00 PM.
@@ -24,5 +26,22 @@ public class CustomEntity extends EntityZombie {
        this.setCustomNameVisible(true);
        this.setBaby(true);
        this.getAttributeInstance((GenericAttributes.MOVEMENT_SPEED)).setValue(100.0D);
+    }
+
+    protected Item getLoot(){
+        return Items.DIAMOND;
+    }
+
+    public Zombie spawn(Location loc){
+        World aWorld = (World) ((CraftWorld)loc.getWorld()).getHandle();
+
+        CustomEntity entity = new CustomEntity(aWorld);
+        entity.setLocation(loc.getX(), loc.getY(), loc.getZ(), loc.getYaw(), loc.getPitch());
+
+        ((CraftLivingEntity) entity.getBukkitEntity()).setRemoveWhenFarAway(false);
+
+        aWorld.addEntity(entity);
+
+        return (Zombie) entity.getBukkitEntity();
     }
 }
